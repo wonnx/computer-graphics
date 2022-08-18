553f3d0ceed8bb0854f220a9d4f60c9f81f982eb
김종원 <labora7@hanyang.ac.kr>
2022-08-18 10:33:00 +0900

    LabAssignment3

---
+++ LabAssignment3/1/HW 3-1.py
@@ -0,0 +1,60 @@
+import glfw
+from OpenGL.GL import *
+import numpy as np
+
+def render(T):
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    # draw cooridnate
+    glBegin(GL_LINES)
+    glColor3ub(255, 0, 0)
+    glVertex2fv(np.array([0.,0.]))
+    glVertex2fv(np.array([1.,0.]))
+    glColor3ub(0, 255, 0)
+    glVertex2fv(np.array([0.,0.]))
+    glVertex2fv(np.array([0.,1.]))
+    glEnd()
+    # draw triangle
+    glBegin(GL_TRIANGLES)
+    glColor3ub(255, 255, 255)
+    glVertex2fv( (T @ np.array([.0,.5,1.]))[:-1] )
+    glVertex2fv( (T @ np.array([.0,.0,1.]))[:-1] )
+    glVertex2fv( (T @ np.array([.5,.0,1.]))[:-1] )
+    glEnd()
+
+def key_callback(window, key, scancode, action, mods):
+    pass
+
+def main():
+    
+    if not glfw.init():
+        return
+    window = glfw.create_window(480,480,"2017029470", None,None)
+    if not window:
+        glfw.terminate()
+        return
+
+    #glfw.set_key_callback(window, key_callback)
+    glfw.make_context_current(window)
+    
+    while not glfw.window_should_close(window):
+        glfw.poll_events()
+
+        t=glfw.get_time()
+        
+        R = np.array([[np.cos(t), -np.sin(t),0.],
+                      [np.sin(t), np.cos(t),0.],
+                      [0., 0., 1.]])
+        
+        T = np.array([[1.,0.,.5],
+                      [0.,1.,.0],
+                      [0.,0.,1.]])
+        
+        render(R @ T)
+        
+        glfw.swap_buffers(window)
+        
+    glfw.terminate()
+if __name__ == "__main__":
+  main()
+
